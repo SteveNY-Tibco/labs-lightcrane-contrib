7b3add59237ce9d8a57aee1c3d40df56d34db11b
Infospecktra <Infospecktra@gmail.com>
2021-11-20 16:59:50 -0500

    Update activity.go

--- activity/docker/deploymonitor/activity.go
+++ activity/docker/deploymonitor/activity.go
@@ -2,7 +2,6 @@ package deploymonitor
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"time"
 
@@ -79,8 +78,8 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 		containerName := container.Names[0]
 		if strings.HasPrefix(containerName, "/Air-") {
 			currentDeploymnts = append(currentDeploymnts, map[string]interface{}{
-				"ProjectID":    containerName[0:strings.Index(containerName, "_")],
-				"Name":         containerName[0:strings.Index(containerName, "_")],
+				"ProjectID":    containerName[1:strings.Index(containerName, "_")],
+				"Name":         containerName[strings.Index(containerName, "_")+1:],
 				"Status":       container.Status,
 				"LastModified": time.Now().Unix(),
 			})
@@ -93,77 +92,6 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	if err != nil {
 		return false, err
 	}
-
-	return true, nil
-}
-
-func (a *Activity) Evalx(ctx activity.Context) (done bool, err error) {
-	ctx.Logger().Info("(fnAirDeployMonitor:Eval) entering ........ ")
-	defer ctx.Logger().Info("(fnAirDeployMonitor:Eval) exit ........ ")
-
-	input := &Input{}
-
-	err = ctx.GetInputObject(input)
-
-	if err != nil {
-		return true, err
-	}
-
-	data := make([]interface{}, 1)
-	data[0] = map[string]interface{}{
-		"ProjectID": "Air-account_00001",
-		"Name":      "edgex_mqtt_mqtt_fs",
-	}
-
-	deployments := make(map[string]interface{})
-	deployments["Update"] = make([]interface{}, 0)
-	deployments["Remove"] = make([]interface{}, 0)
-
-	/*
-		ProjectID
-		Name
-		Data {}
-		Status
-		LastModified
-		ErrorCode
-		ErrorMessage
-	*/
-	registeredDeployments := make(map[string]interface{})
-	if nil != data {
-		for _, registeredDeployment := range data {
-			projectID := registeredDeployment.(map[string]interface{})["ProjectID"]
-			name := registeredDeployment.(map[string]interface{})["Name"]
-			registeredDeployments[fmt.Sprintf("/%s_%s", projectID, name)] = registeredDeployment
-		}
-	}
-
-	fmt.Println("(fnAirDeployMonitor:Eval) registered deployments : ", registeredDeployments)
-
-	/* query docker container */
-	dctx := context.Background()
-	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-	if err != nil {
-		return
-	}
-
-	containers, err := cli.ContainerList(dctx, types.ContainerListOptions{All: true})
-	if err != nil {
-		return
-	}
-
-	currentDeploymnts := make(map[string]interface{})
-	for _, container := range containers {
-		fmt.Println(container.Names[0] + "-" + container.Status)
-		currentDeploymnts[container.Names[0]] = container
-		registeredDeployment := registeredDeployments[container.Names[0]]
-		if nil != registeredDeployment {
-			registeredDeployment.(map[string]interface{})["Status"] = container.Status
-			deployments["Update"] = append(deployments["Update"].([]interface{}), registeredDeployment)
-		}
-	}
-
-	ctx.Logger().Info("(fnAirDeployMonitor:Eval) currentDeploymnts : ", currentDeploymnts)
-	ctx.Logger().Info("(fnAirDeployMonitor:Eval) deployments : ", deployments)
 
 	return true, nil
 }
