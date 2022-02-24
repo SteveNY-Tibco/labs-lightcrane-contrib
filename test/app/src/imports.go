cb4d956431abd3ceeaed7494ebb95a9069395737
Infospecktra <Infospecktra@gmail.com>
2022-02-23 23:13:51 -0500

    update

--- test/app/src/imports.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	_ "github.com/TIBCOSoftware/labs-air-contrib/activity/log"
-	_ "github.com/project-flogo/flow"
-	_ "github.com/project-flogo/contrib/activity/rest"
-	_ "github.com/project-flogo/contrib/function/datetime"
-	_ "github.com/project-flogo/contrib/trigger/timer"
-)
